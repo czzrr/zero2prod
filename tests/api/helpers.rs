74def50458bd7dbc326b53938b4b1a44ded43253
Christopher Acosta <chriz@live.dk>
2023-01-27 13:12:00 +0100

    Pending confirmation when subscribed

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -2,6 +2,7 @@ use once_cell::sync::Lazy;
 use secrecy::{ExposeSecret, Secret};
 use sqlx::types::Uuid;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
+use wiremock::MockServer;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::{get_connection_pool, Application};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -23,6 +24,7 @@ static TRACING: Lazy<()> = Lazy::new(|| {
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
+    pub email_server: MockServer,
 }
 
 impl TestApp {
@@ -39,6 +41,9 @@ impl TestApp {
 
 pub async fn spawn_app() -> TestApp {
     Lazy::force(&TRACING);
+
+    let email_server = MockServer::start().await;
+
     // Randomise configuration to ensure test isolation
     let configuration = {
         let mut c = get_configuration().expect("Failed to read configuration.");
@@ -46,8 +51,11 @@ pub async fn spawn_app() -> TestApp {
         c.database.database_name = Secret::new(Uuid::new_v4().to_string());
         // Use a random OS port
         c.application.port = 0;
+        // Use the mock server as email API
+        c.email_client.base_url = email_server.uri();
         c
     };
+
     // Create and migrate the database
     configure_database(&configuration.database).await;
     // Launch the application as a background task
@@ -60,6 +68,7 @@ pub async fn spawn_app() -> TestApp {
     TestApp {
         address,
         db_pool: get_connection_pool(&configuration.database),
+        email_server,
     }
 }
 
